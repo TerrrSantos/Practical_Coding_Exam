067ac50628bb831a34e130bea5746650308af5ce
TerrrSantos <59819455+TerrrSantos@users.noreply.github.com>
2020-01-13 15:50:24 +0800

    Connected the database using MySQL successfully

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
+//Start MySQL Connection
+var connection = mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'password',
+        database:'questronix',
+        multipleStatements:true
+    }
+)
+
+connection.connect((err)=>
+    {
+        if(!err)
+        {
+            console.log("Connected to Questronix Database")
+        }
+        else
+        {
+            console.log("Connection Failed");
+        }
+    }
+)
+//End of MySQL Connection
+
 app.listen(3000,()=>
     {
         console.log("Server is up");
